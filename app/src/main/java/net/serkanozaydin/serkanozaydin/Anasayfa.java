de7932e7a0cac8981925d247cb317714ac5c855d
Serkan <serkan.zaydn@gmail.com>
2017-01-29 03:56:41 +0300

    Python kategorisi eklendi

--- app/src/main/java/net/serkanozaydin/serkanozaydin/Anasayfa.java
+++ app/src/main/java/net/serkanozaydin/serkanozaydin/Anasayfa.java
@@ -33,7 +33,7 @@ import static android.content.Context.MODE_PRIVATE;
 public class Anasayfa extends Fragment {
 
 
-    public ImageButton Anasayfa, BenKimim, Projelerim, WebUgra, Yeteneklerim, Linux, Android, RaspberryPi, KisiselDeneyimlerim, Facebook, Twitter, Github, Instagram, Android_Sosyal, Youtube;
+    public ImageButton Anasayfa,Python,BenKimim, Projelerim, WebUgra, Yeteneklerim, Linux, Android, RaspberryPi, KisiselDeneyimlerim, Facebook, Twitter, Github, Instagram, Android_Sosyal, Youtube;
     FragmentManager fragmentManager;
 
     @Nullable
@@ -55,6 +55,55 @@ public class Anasayfa extends Fragment {
         Instagram = (ImageButton) root.findViewById(R.id.btnInstagram);
         Android_Sosyal = (ImageButton) root.findViewById(R.id.btnAndroidSosyal);
         Youtube = (ImageButton) root.findViewById(R.id.btnYoutube);
+        Python= (ImageButton) root.findViewById(R.id.btnPython);
+
+
+
+        Python.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_UP: {
+                        Toast.makeText(getContext(), "Python", Toast.LENGTH_SHORT).show();
+                        ImageButton view = (ImageButton) v;
+                        view.getBackground().clearColorFilter();
+                        view.invalidate();
+                        fragmentManager = getActivity().getSupportFragmentManager();
+                        FragmentTransaction transaction = fragmentManager.beginTransaction();
+                        AnaEkran fragment = new AnaEkran();
+                        transaction.replace(R.id.container, fragment, "deneme");
+                        transaction.addToBackStack(null);
+                        transaction.commit();
+                        Model model = new Model("https://www.serkanozaydin.net/python/");
+                        SharedPreferences mPrefs = getActivity().getPreferences(MODE_PRIVATE);
+                        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+                        Gson gson = new Gson();
+                        String json = gson.toJson(model);
+                        prefsEditor.putString("MyObject", json);
+                        prefsEditor.commit();
+
+
+                        break;
+                    }
+                    case MotionEvent.ACTION_DOWN: {
+                        ImageButton view = (ImageButton) v;
+                        view.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_ATOP);
+                        view.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_CANCEL: {
+                        ImageButton view = (ImageButton) v;
+                        view.getBackground().clearColorFilter();
+                        view.invalidate();
+                        break;
+
+                    }
+                }
+
+
+                return true;
+            }
+        });
 
 
         Anasayfa.setOnTouchListener(new View.OnTouchListener() {
